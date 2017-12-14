78a38724df7b4443bfb58a48f3124e06dfa82e6f
Hammad Hai <hammad.a.hai@gmail.com>
2017-12-14 10:01:14 -0500

    Add new watcher for credentials report.

---
+++ security_monkey/watchers/custom/iam_cred_report.py
@@ -0,0 +1,67 @@
+import csv
+
+from security_monkey.watcher import Watcher
+from security_monkey.watcher import ChangeItem
+from security_monkey.exceptions import (
+    BotoConnectionIssue,
+    CredentialReportException,
+)
+
+class CredentialReportWatcher(Watcher):
+    index = 'credreport'
+    i_am_singular = 'Credential Report'
+    i_am_plural = 'Credential Reports'
+
+    def __init__(self, accounts=None, debug=False):
+        super(CredentialReport, self).__init__(accounts=accounts, debug=debug)
+
+    def slurp(self):
+        """
+        :returns: item_list - list of credential reports.
+        :returns: exception_map - A dict where the keys are a tuple containing the
+            location of the exception and the value is the actual exception
+        """
+        self.prep_for_slurp()
+        item_list = []
+        exception_map = {}
+
+        from security_monkey.common.sts_connect import connect
+        for account in self.accounts:
+            try:
+                iam = connect(account, 'boto3.iam.client')
+            except Exception as e:
+                exc = BotoConnectionIssue(str(e), 'iamcredreport', account, None)
+                self.slurp_exception((self.index, account, 'universal'), exc, exception_map,
+                                     source="{}-watcher".format(self.index))
+                continue
+
+            response = iam.get_credential_report()
+            app.logger.debug('Getting credential report for account {}'.format(account))
+
+            try:
+                credential_report = csv.DictReader(open(response['Content'], 'rb'))
+            except Exception as e:
+                exc = CredentialReportException(str(e), 'iamcredreport', account, None)
+                self.slurp_exception((self.index, account, 'universal'), exc, exception_map,
+                                     source="{}-watcher".format(self.index))
+                continue
+
+            for user_report in credential_report:
+                item_list.append(
+                    UserReport(
+                        account=account,
+                        config=user_report,
+                    )
+                )
+
+        return item_list, exception_map
+
+
+class UserReport(ChangeItem):
+    def __init__(self, account=None, name=None, region=None, config={}):
+        super(UserReport, self).__init__(
+                index=CredentialReportWatcher.index,
+                region=region,
+                account=account,
+                name=name,
+                new_config=config)
