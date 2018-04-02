07cb6cfcb8ac69da0f96499c596e19dc9ef142bf
Hammad Hai <hammad.a.hai@gmail.com>
2018-04-02 14:57:57 -0400

    Add checks for rules 3.x

--- security_monkey/auditors/cloudtrail.py
+++ security_monkey/auditors/cloudtrail.py
@@ -42,31 +42,3 @@ class CloudTrailAuditor(Auditor):
             message = "POLICY - CloudTrail is disabled"
             self.add_issue(10, message, cloud_trail)
         return False
-
-    def check_2_4_cloudwatch_logs_integration(self, cloud_trail):
-        """
-        CIS Rule 2.4 - Ensure CloudTrail trails are integrated with CloudWatch
-        Logs (Scored)
-        """
-        issue = Categories.INFORMATIONAL
-        notes = Categories.INFORMATIONAL_NOTES.format(
-            description='sa-log-cis-2.4 - ',
-            specific='CloudTrails without CloudWatch Logs discovered.'
-        )
-
-        logs_arn = cloud_trail.config.get('cloudwatch_logs_log_group_arn')
-        if not logs_arn or 'arn:aws:logs' not in logs_arn:
-            self.add_issue(10, issue, cloud_trail, notes=notes)
-
-    def check_2_7_logs_encrypted(self, cloud_trail):
-        """
-        CIS Rule 2.7 - Ensure CloudTrail logs are encrypted at rest using KMS
-        CMKs (Scored)
-        """
-        issue = Categories.INFORMATIONAL
-        notes = Categories.INFORMATIONAL_NOTES.format(
-            description='sa-log-cis-2.7 - ',
-            specific='CloudTrail not using KMS CMK for encryption discovered.'
-        )
-        if not cloud_trail.config.get('kms_key_id'):
-            self.add_issue(10, issue, cloud_trail, notes=notes)
